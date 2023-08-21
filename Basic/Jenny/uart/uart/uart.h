e48a1631f791e605238e822d4878ab067766e28e
jwan404 <jwan404@aucklanduni.ac.nz>
2023-08-21 19:17:11 +1200

    Update uart.h

--- Basic/Jenny/uart/uart/uart.h
+++ Basic/Jenny/uart/uart/uart.h
@@ -12,5 +12,4 @@
 void uart_init(uint32_t baud_rate); // sets baud rate
 void uart_transmit_byte(char byte); // transmit byte
 void uart_transmit_array(char* msg); // transmit array
-void extract_digits(int number, char *digit1, char *digit2, char *digit3); // extracts each digit
 #endif /* UART_H_ */
