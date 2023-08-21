277918bfb70c38e22df9c573cbe60a1792de8523
jwan404 <jwan404@aucklanduni.ac.nz>
2023-08-21 15:55:31 +1200

    main.c

---
+++ Basic/Jenny/uart/uart/main.c
@@ -0,0 +1,18 @@
+/*
+ * uart.c
+ *
+ * Created: 21/08/2023 3:53:25 pm
+ * Author : Jenny W
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
