d9bccf737d054fbcca1c1f94bb2891043cef4245
random-sk <126382798+random-sk@users.noreply.github.com>
2023-10-10 17:19:03 +1300

    in 209Project5, added code to find V, I, P

---
+++ Basic/Seibon/209Project5/209Project/209Project/adc.c
@@ -0,0 +1,40 @@
+/*
+ * adc.c
+ *
+ * Created: 10/10/2023 4:04:30 pm
+ *  Author: skim641
+ */ 
+
+//Code from ADC lab
+
+#include "adc.h"
+
+#include <avr/io.h>
+#include <stdint.h>
+
+void adc_init(){
+	ADMUX = 0b01000010;
+	ADCSRA = 0b10000100;
+	ADCSRB = 0b00000000;
+	DIDR0 = 0b00000000;
+}
+
+uint16_t adc_read(uint8_t chan){
+	//Set channel
+	ADMUX &= 0xF0;
+	if (chan == 1){
+		ADMUX |= 0x01;
+		}else if (chan == 0){
+		ADMUX |= 0x00;
+		}else{
+		ADMUX |= 0x02;
+	}
+	//Start ADC conversion
+	ADCSRA |= (1 << ADSC);
+	//Polls for conversion to finish
+	while((ADCSRA & (1<<ADIF)) == 0){
+		;
+	}
+	//Read+return ADC result
+	return ((ADCL << 0) | (ADCH << 8));
+}
---
+++ Basic/Seibon/209Project5/209Project/209Project/main.c
@@ -0,0 +1,48 @@
+/*
+ * 209Project.c
+ *
+ * Created: 10/10/2023 4:04:06 pm
+ * Author : skim641
+ */ 
+
+//Our .h files
+#include "uart.h"
+#include "adc.h"
+
+//Libraries
+#include <avr/io.h>
+
+
+int main(void)
+{
+	//Initializing registers:
+	uart_init();
+	adc_init();
+	DDRB = 0b00110011;
+	DDRC = 0b01111111;
+	DDRD = 0b00000000;
+	
+	//Initializing variables:
+	volatile uint8_t channel; //for ADC
+	volatile uint16_t voltage;
+	volatile uint16_t current;
+	volatile uint16_t power;
+	volatile uint8_t digit[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F}; //segment patterns
+		
+	
+	/* Replace with your application code */
+	while (1)
+	{
+		//Get voltage, current and power from ADC
+		channel = 0;
+		voltage = adc_read(channel);
+		channel = 1;
+		current = adc_read(channel)/0.5; //Voltage across shunt resistor, divided by shunt resistor value
+		power = voltage * current;
+		
+		//Displaying info on LCD
+		
+		
+		//Displaying info via UART
+	}
+}
