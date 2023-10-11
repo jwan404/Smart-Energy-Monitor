f33516601c4194affe3320c6e85c397199aa13c5
jwan404 <jwan404@aucklanduni.ac.nz>
2023-10-11 21:16:13 +1300

    Update main.c

--- Basic/Jenny/209Project/209Project/main.c
+++ Basic/Jenny/209Project/209Project/main.c
@@ -13,7 +13,12 @@
 
 //Libraries
 #include <avr/io.h>
-volatile uint16_t Vac[101] = {};
+#include <math.h>
+
+#define NUM_SAMPLES 100
+
+volatile uint16_t Vac[NUM_SAMPLES] = {};
+float Vref = 2.1;  // Assuming a 2.1V reference voltage
 
 int main(void)
 {
@@ -21,17 +26,39 @@ int main(void)
 	adc_init();
 	timer0_init();
 	timer1_init();
+	init_display();
     /* Replace with your application code */
     while (1) 
     {
-		// Read ADC value from channel 2 (modify as needed)
-		for (int i = 0; i < 100; i++)
-		{
-			Vac[i] = (ADC0Value[i] * (5/1024) - 2.1) / (Gvs*Gvo); // Display this value
-		}
-		uint16_t adc_result = adc_read_channel_single_conversion(0);
-		uint16_t adc_result = adc_read_channel_single_conversion(1);
+	
+		 for (int i = 0; i < NUM_SAMPLES; i++)
+		 {
+			 uint16_t adc_result_ch0 = adc_read_channel_single_conversion(0);
+			 uint16_t adc_result_ch1 = adc_read_channel_single_conversion(1);
 
-    }
+			 // Calculate voltage for each channel
+			 float voltage_ch0 = adc_result_ch0 * Vref / 1024.0;
+			 float voltage_ch1 = adc_result_ch1 * Vref / 1024.0;
+
+			 // Calculate Vac for the i-th sample
+			 Vac[i] = (voltage_ch0 - voltage_ch1) / (Gvs * Gvo);
+		 }
+
+		 // Calculate Vrms using Riemann sum
+		 float sum = 0.0;
+		 for (int i = 0; i < NUM_SAMPLES; i++) {
+			 sum += Vac[i] * Vac[i];
+		 }
+
+		 float Vrms = sqrt(sum / NUM_SAMPLES);
+		 
+		 // Load Vrms value into the display buffer
+		 separate_and_load_characters((uint16_t)(Vrms * 100), 2);
+
+		 // Send the characters to the display
+		 send_next_character_to_display();
+	 }
+	 
+	  
 }
 
