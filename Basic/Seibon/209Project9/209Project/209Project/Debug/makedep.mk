63a763e15a58ac59de5ffb377639d9045e0082c7
random-sk <126382798+random-sk@users.noreply.github.com>
2023-10-13 16:08:26 +1300

    Made code alternate flashing between V and I

---
+++ Basic/Seibon/209Project9/209Project/209Project/Debug/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc.c
+
+display.c
+
+energyCalculations.c
+
+main.c
+
+timer0.c
+
+timer1.c
+
+uart.c
+
---
+++ Basic/Seibon/209Project9/209Project/209Project/timer0.h
@@ -0,0 +1,14 @@
+/*
+ * timer0.h
+ *
+ * Created: 10/10/2023 6:08:09 pm
+ *  Author: Jenny W
+ */ 
+
+
+#ifndef TIMER0_H_
+#define TIMER0_H_
+
+void timer0_init();
+
+#endif /* TIMER0_H_ */
